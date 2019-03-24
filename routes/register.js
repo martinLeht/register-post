b4acc11219aa048639ffedc7f779c6c68be5c62a
Martin Lehtomaa <martin.lehtomaa@gmail.com>
2019-03-24 17:46:55 +0200

    Added error checking to registration form.

--- routes/register.js
+++ routes/register.js
@@ -7,4 +7,40 @@ router.get('/', (req, res) => {
     });
 });
 
+router.post('/', (req, res) => {
+    const {
+        firstname,
+        lastname,
+        email,
+        password,
+        confirmPass
+    } = req.body;
+    let errors = [];
+
+    // Check fields
+    if (!name || !email || !password || !confirmPass) {
+        errors.push({msg: 'Please fill in all fields'})
+    }
+
+    // Check if passwords match
+    if (password !== confirmPass) {
+        errors.push({msg: 'Passwords do not match'})
+    }
+
+    // Check password length
+    if (password.length < 6) {
+        errors.push({msg: 'Password has to be atleast 6 characters'});
+    }
+
+    if(errors.length >0) {
+        res.render('register', {
+            title: "Register | Info Point",
+            errors: errors
+        });
+    } else {
+        req.flash("success_msg");
+        res.redirect("/login");
+    }
+});
+
 module.exports = router;
